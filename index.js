3cac3d878fae898dcb60dccf69c1bd559d462758
kader009 <kadermollarasel000@gmail.com>
2023-10-07 12:06:53 +0600

    add new header picture

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
+// gsap animation
+
+
+
+gsap.from('nav',{
+  y:15,
+  delay:0.4,
+  duration:0.8,
+  ease:'slow'
+})
+
+
+gsap.from('.heade-text, h1, h2, h4, .btn-div', {
+  y:100,
+  delay:0.9,
+  duration:0.5,
+  stagger:0.1,
+  ease:'slow.out'
+});
+
+
+
+
 
 
 // progress bar
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  document.getElementById('myBar').style.width = scrolled + '%';
 }
-
 
 // image scren functions
 function openImg(imgName) {
   var i, x;
-  x = document.getElementsByClassName("picture");
+  x = document.getElementsByClassName('picture');
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].style.display = 'none';
   }
-  document.getElementById(imgName).style.display = "block";
+  document.getElementById(imgName).style.display = 'block';
 }
+
 
 
 // aos initial
 AOS.init();
-
-
-
-
-
